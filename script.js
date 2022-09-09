07144ca95e81b18c78628dd7abf6364ee2741128
Fabiano Novais <fabiano.novais.99@gmail.com>
2022-09-08 21:52:00 -0300

    Aula + desafios juntos, adicionando um comparativo se o número chutado é maior ou menor que o numero secreto

---
+++ script.js
@@ -0,0 +1,23 @@
+//O computador tá escolhendo um número aleatório
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+    let numeroInput = parseInt(document.getElementById("valor").value);
+    let resultado = document.getElementById("resultado");
+
+    if (numeroInput == numeroSecreto) {
+        resultado.innerHTML = "Acertou, Mizeravi!! " + numeroSecreto + " é o número secreto!";
+        document.getElementById("resultado").style.color = "green";
+    } else if (numeroInput > 10 || numeroInput < 0) {
+        resultado.innerHTML = "Digite um valor entre 0 e 10";
+        document.getElementById("resultado").style.color = "red";
+
+    } else if(numeroInput > numeroSecreto){
+        resultado.innerHTML = "Errouuuuu!! " + numeroInput + " é maior que o número secreto!!";
+        document.getElementById("resultado").style.color = "red";
+    } else if(numeroInput < numeroSecreto){
+        resultado.innerHTML = "Errouuuuu!! " + numeroInput + " é menor que o número secreto!!";
+        document.getElementById("resultado").style.color = "red";
+
+    }
+}
